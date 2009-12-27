5c7830c802e886d946e102ea56997057b6c7741c
Theoretical <mdeception@gmail.com>
2009-12-27 15:41:43 -0500

    Login now gives out proper flags; Character List done.

--- MatchServer/Packet/Handle/Match.cs
+++ MatchServer/Packet/Handle/Match.cs
@@ -36,11 +36,14 @@ namespace MatchServer.Packet.Handle
                 result = Results.LoginIncorrectPassword;
             else
             {
+                Log.Write("Login: {0} {1}", UserID, Password);
                 Database.GetAccount(UserID, Password, ref client.mAccount);
                 if (client.mAccount.nAID == 0)
                     result = Results.LoginIncorrectPassword;
                 else if (client.mAccount.nUGradeID == MMatchUserGradeID.Banned || client.mAccount.nUGradeID == MMatchUserGradeID.Penalty)
                     result = Results.LoginBannedID;
+                else if (client.mAccount.nAID > 0)
+                    client.mClientFlags = PacketFlags.Login;
             }
             PacketWriter pLoginResponse = new PacketWriter(Operation.MatchLoginResponse, CryptFlags.Encrypt);
             pLoginResponse.Write((Int32)result);
@@ -56,7 +59,17 @@ namespace MatchServer.Packet.Handle
             client.Send(pLoginResponse);
 
             if (client.mAccount.nUGradeID == MMatchUserGradeID.Banned || client.mAccount.nUGradeID == MMatchUserGradeID.Penalty)
-                client.Disconnect();
+                client. Disconnect();
+        }
+
+        [PacketHandler(Operation.MatchRequestAccountCharList, PacketFlags.Login)]
+        public static void ProcessCharacters(Client pClient, PacketReader pPacket)
+        {
+            PacketWriter pResponseChars = new PacketWriter(Operation.MatchResponseAccountCharList, CryptFlags.Encrypt);
+            var charCount = Database.GetQuery(string.Format("SELECT COUNT(*) FROM Character WHERE AID={0}", pClient.mAccount.nAID));
+            pResponseChars.Write(charCount, 34);
+            Database.GetCharacterList(pClient.mAccount.nAID, pResponseChars);
+            pClient.Send(pResponseChars);
         }
     }
 }
