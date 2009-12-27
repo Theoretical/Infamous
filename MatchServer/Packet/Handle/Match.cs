5ae9e381849812ad998c4477e33f9d84536ba751
Theoretical <mdeception@gmail.com>
2009-12-27 14:19:26 -0500

    Built Login Response packet & sent it.

--- MatchServer/Packet/Handle/Match.cs
+++ MatchServer/Packet/Handle/Match.cs
@@ -9,6 +9,54 @@ namespace MatchServer.Packet.Handle
         [PacketHandler(Operation.MatchLogin, PacketFlags.None)]
         public static void ProcessLogin(Client client, PacketReader pPacket)
         {
+            /* @ARGS
+             * UserID - string
+             * Password - string
+             * CommandVersion - int
+             * Checksum - int
+             * MD5 - Blob
+             */
+
+            var UserID = pPacket.ReadString();
+            var Password = pPacket.ReadString();
+            var CommandID = pPacket.ReadInt32();
+            var Checksum = pPacket.ReadInt32();
+            var totalSize = pPacket.ReadInt32();
+            var blobSize = pPacket.ReadInt32();
+            var count = pPacket.ReadInt32();
+            Results result = Results.Accepted;
+
+            if (blobSize > 32)
+            {
+                client.Disconnect();
+                return;
+            }
+
+            if (!Program.mRegex.IsMatch(UserID) || !Program.mRegex.IsMatch(Password))
+                result = Results.LoginIncorrectPassword;
+            else
+            {
+                Database.GetAccount(UserID, Password, ref client.mAccount);
+                if (client.mAccount.nAID == 0)
+                    result = Results.LoginIncorrectPassword;
+                else if (client.mAccount.nUGradeID == MMatchUserGradeID.Banned || client.mAccount.nUGradeID == MMatchUserGradeID.Penalty)
+                    result = Results.LoginBannedID;
+            }
+            PacketWriter pLoginResponse = new PacketWriter(Operation.MatchLoginResponse, CryptFlags.Encrypt);
+            pLoginResponse.Write((Int32)result);
+            pLoginResponse.Write("Lol Emu Test");
+            pLoginResponse.Write((byte)2);//Moode
+            pLoginResponse.Write(UserID);
+            pLoginResponse.Write((byte)client.mAccount.nUGradeID);
+            pLoginResponse.Write((byte)client.mAccount.nPGradeID);
+            pLoginResponse.Write(client.mClientUID);
+            pLoginResponse.Write(false);
+            pLoginResponse.Write(1, 20);
+            pLoginResponse.WriteSkip(20);
+            client.Send(pLoginResponse);
+
+            if (client.mAccount.nUGradeID == MMatchUserGradeID.Banned || client.mAccount.nUGradeID == MMatchUserGradeID.Penalty)
+                client.Disconnect();
         }
     }
 }
