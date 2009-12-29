3d93e5cafd8f1bb16c99b1c15e39c7324ea5dbb8
Theoretical <mdeception@gmail.com>
2009-12-28 23:17:15 -0500

    Added channel manager, handles join so far.

---
+++ MatchServer/Manager/ChannelMgr.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using MatchServer.Core;
+using MatchServer.Packet;
+namespace MatchServer.Manager
+{
+    class ChannelMgr
+    {
+        public static void Join (Client pClient, MMatchChannel pChannel)
+        {
+            lock (pChannel.lClients)
+                pChannel.lClients.Add(pClient);
+
+            pClient.mPlace = MMatchPlace.Lobby;
+            pClient.mChannel = pChannel;
+
+            PacketWriter pPacket = new PacketWriter(Operation.ChannelResponseJoin, CryptFlags.Encrypt);
+            pPacket.Write(pChannel.uidChannel);
+            pPacket.Write((Int32)pChannel.nChannelType);
+            pPacket.Write(pChannel.szName);
+            pPacket.Write(true);
+            pClient.Send(pPacket);
+
+            pPacket = new PacketWriter(Operation.MatchResponseRuleset, CryptFlags.Encrypt);
+            pPacket.Write(pChannel.uidChannel);
+            pPacket.Write(pChannel.nChannelRule.ToString());
+            pClient.Send(pPacket);
+        }
+    }
+}
