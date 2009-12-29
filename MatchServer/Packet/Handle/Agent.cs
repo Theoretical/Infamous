531d7e12c12744322677fbb9b1bb2b7795894f84
Theoretical <mdeception@gmail.com>
2009-12-29 07:47:39 -0500

    Added MatchAgent..

---
+++ MatchServer/Packet/Handle/Agent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using MatchServer.Core;
+using MatchServer.Packet;
+using MatchServer.Manager;
+
+namespace MatchServer.Packet.Handle
+{
+    class Agent
+    {
+        [PacketHandler(Operation.MatchRegisterAgent, PacketFlags.None)]
+        public static void ProcessRegisterAgent (Client pClient, PacketReader pPacket)
+        {
+            pClient.mIsAgent = true;
+            Log.Write("[{0}] Agent Registed.", pClient.mClientIP);
+            lock (Program.mAgents)
+                Program.mAgents.Add(pClient);
+        }
+
+        [PacketHandler(Operation.MatchAgentRequestLiveCheck, PacketFlags.None)]
+        public static void ProcessLiveCheck (Client pClient, PacketReader pPacket)
+        {
+            var timeStamp = pPacket.ReadInt32();
+            PacketWriter pResponseLiveCheck = new PacketWriter(Operation.MatchAgentResponseLiveCheck, CryptFlags.Encrypt);
+            pResponseLiveCheck.Write(timeStamp);
+            pClient.Send(pResponseLiveCheck);
+        }
+    }
+}
