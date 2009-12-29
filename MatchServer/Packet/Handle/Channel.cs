e95a4c2869223928100549280bd1e7923d70e58c
Theoretical <mdeception@gmail.com>
2009-12-28 23:17:46 -0500

    Added channel handler

---
+++ MatchServer/Packet/Handle/Channel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using MatchServer.Core;
+using MatchServer.Packet;
+using MatchServer.Manager;
+namespace MatchServer.Packet.Handle
+{
+    class Channel
+    {
+        [PacketHandler(Operation.ChannelJoin, PacketFlags.Character)]
+        public static void ResponseChannelJoin (Client pClient, PacketReader pPacket)
+        {
+            var uidChar = pPacket.ReadUInt64();
+            var uidChan = pPacket.ReadUInt64();
+
+            if (uidChar != pClient.mClientUID)
+            {
+                pClient.Disconnect();
+                return;
+            }
+
+            MMatchChannel channel = Program.mChannels.Find(c => c.uidChannel == uidChan);
+            if (channel != null)
+                ChannelMgr.Join(pClient, channel);
+        }
+    }
+}
